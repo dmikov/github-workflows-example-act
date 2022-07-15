5d1a126247c95db44dde3ae35b2d691a52acfaed
Dmitriy Krasnikov <dmikov@gmail.com>
2022-07-15 08:30:35 -0400

    Work on accessing bitwarden.

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM nektos/act-environments-ubuntu:18.04
+RUN npm install -g @bitwarden/cli
---
+++ run-act-bitwarden.sh
@@ -0,0 +1,1 @@
+act workflow_dispatch -W .github/workflows/bitwarden.yml -e .github/debug/bitwarden.event.json --secret-file .github/debug/bitwarden.secret
